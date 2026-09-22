eeba68eabe3930c814059088baffe823d4c9a69d
tuan_muda <rumasoreng757@gmail.com>
2026-09-23 00:49:11 +0700

    Add files via upload

---
+++ vidownload.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+clear
+		echo -e "\033[0;35m◢◤━━━━━━━━━━━━━━━━━━━━━━◥◣\033[0m"
+		echo -e "\033[1;36m    H O T  V I D E O    \033[0m"
+		echo -e "\033[1;36m   D O W N L O A D E R !\033[0m"
+		echo -e "\033[0;35m◥◣━━━━━━━━━━━━━━━━━━━━━━◢◤\033[0m"
+		echo -e "\033[1;33m [!] https://github.com/iksan757\033[0m"
+		echo -e "\033[1;33m [!] Rumasoreng757@gmail.com\033[0m"
+		echo ""
+
